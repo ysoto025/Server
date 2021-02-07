e9799ed1c3a6fff819a13387055fbc908051c4d7
ysoto025 <55642058+ysoto025@users.noreply.github.com>
2021-02-06 19:18:17 -0500

    Update server-s.py

--- server-s.py
+++ server-s.py
@@ -1,6 +1,8 @@
 import sys
 import socket
 from sys import argv
+import threading
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.settimeout(10.0)
@@ -9,21 +11,37 @@ if (len(argv) < 2) | (len(argv) > 2):
     sys.stderr.write("missing arguments or too many Arguments")
     sys.exit()
 
-if (argv[1] == ""):
+if argv[1] == "":
     sys.stderr.write("ERROR: empty string")
-    sys.exit()
+    sys.exit(1)
 
 if (int(argv[1]) < 0) | (int(argv[1]) > 65535):
     sys.stderr.write("ERROR: Overflow error")
-    sys.exit()
-
+    sys.exit(1)
 
 con = []
 sock.bind(('0.0.0.0', int(argv[1])))
 sock.listen(1)
+
+
+def connector(d, e):
+    global con
+    while True:
+        a = d.recv(1024)
+        for con in con:
+            con.send("accio\r\n")
+        if not a:
+            break
+
+
 try:
     while True:
         x, v = sock.accept()
+        # sock.send("accio\r\n")
+        cThread = threading.Thread(target=connector, args=(x, v))
+        cThread.daemon = True
+        cThread.start()
+
         con.append(x)
         print(con)
 except socket.timeout:
