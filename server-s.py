bc6ab5a1a80c7bd282618a8466682d4d6d61596e
ysoto025 <55642058+ysoto025@users.noreply.github.com>
2021-02-07 22:08:21 -0500

    Update server-s.py

--- server-s.py
+++ server-s.py
@@ -24,20 +24,19 @@ con = []
 sock.bind(('0.0.0.0', int(argv[1])))
 sock.listen(1)
 data = ''
-var = 0
+
 
 def connector(d, e):
     global con
     global data
     word = 'accio\r\n'
-    global var
-    var = 0
+
     while True:
         d.send(bytes(word.encode()))
-        a = d.recv(1).decode("utf-8")
-        var = var + 1
-        #print(var)
+        a = d.recv(1)
+
         if not a:
+            print(len(a.decode("utf-8")))
             con.remove(d)
             d.close()
             break
@@ -57,7 +56,7 @@ try:
         cThread.daemon = True
         cThread.start()
         con.append(x)
-        print(var)
+
 
 except socket.timeout:
     sys.stderr.write("ERROR: timeout")
